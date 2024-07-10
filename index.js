a06385321b7b557827fecb22ad2983a5c4149821
prajapati-chirag-216 <prajapatichirag2162003@gmail.com>
2024-07-10 16:25:27 +0530

    ready for production testing

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "http://localhost:5000"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+  })
+);
 
 app.use(express.json());
 app.use(bodyParser.json());
