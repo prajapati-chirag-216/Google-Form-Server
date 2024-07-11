7083a26a1b10645444bee1ee98b20f143c4aa9be
prajapati-chirag-216 <prajapatichirag2162003@gmail.com>
2024-07-11 11:24:23 +0530

    added new get route

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json("Welcome to app");
 });
+app.get("/userData", (req, res) => {
+  res.json("This is userData");
+});
 
 app.post("/", (req, res) => {
   console.log(req.body);
